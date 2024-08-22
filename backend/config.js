d5a27683e35141ed78c0c5076debdc08d5155700
SharatRokkam <rokkamsharath1@gmail.com>
2024-08-22 10:56:55 +0530

    bookStore

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5000;
+
+export const mongoDBURL =
+  "mongodb+srv://sharatrokkam:Sharath987@mern-bookstore.1rvaq.mongodb.net/?retryWrites=true&w=majority&appName=MERN-BookStore";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
